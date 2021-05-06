00f771d02031ff077c6afa4df9e2b02d5570a234
Christopher Rose <christophervrose@gmail.com>
2021-05-06 12:36:25 +0530

    [update] add assertions

--- README.md
+++ README.md
@@ -1,15 +1,21 @@
 # xml-servicefile-parser
 
-Parse servicefile.xml from stock Android pieces to create a script to restore to stock
+Parse `servicefile.xml` from stock Android pieces to create a script to restore to stock.
+
+> Remember to *inspect* the created scripts before actually running them.
+
 
 ## Executable
 
 ### Building
 
-1. `npm i`
-2. `npm run package`
+1. `npm i` -> This will install the dependencies
+2. Edit `settings.json` accordingly.
+3. `npm run pkg` -> This will package the application with the configured settings.json
 
 ### Usage
 
 1. Place alongside `servicefile.xml`.
-2. Run executable or `node test`, if using source.
+2. Run executable.
+> Be aware that the default executable creates `serviceScript.sh` and `serviceScript.bat`, so don't have any files of that name already. 
+3. Inspect the scripts and use them as required.
--- index.js
+++ index.js
@@ -1,58 +1,104 @@
+const { parseString } = require('xml2js');
+const path = require('path');
+const fs = require('fs');
+const assert = require('assert');
 
-const {parseString} = require('xml2js')
-const path = require('path')
-const fs = require('fs')
 
-const settings = JSON.parse(fs.readFileSync( path.join(__dirname,"settings.json") ))
-console.log("Loaded configuration")
+
+console.log('Loading configuration: settings.json')
+const settings = JSON.parse(
+    fs.readFileSync(path.join(__dirname, 'settings.json'))
+);
+assert(typeof settings?.scriptConfig?.general?.command ==='string','Expected command')
+assert(Array.isArray(settings?.scriptConfig?.env),'Expected an array of environment statuses')
+assert(typeof settings?.fileName==='string', 'Expected a filename');
+assert(typeof settings?.serviceScript==='string','Expected a result filename')
+
+
+console.log('Loaded configuration');
 //console.log(settings)
 //console.log(process.argv)
-fs.readFile(settings.fileName,(err,data)=>{
-    if(err){
-        throw new Error(`${err.code}: Could not read file. Try again. ${err.name}`)
+fs.readFile(settings.fileName, (err, data) => {
+    if (err) {
+        throw new Error(
+            `${err.code}: Could not read file. Try again. ${err.name}`
+        );
     }
-    console.log(`Reading ${settings.fileName}`)
-    parseString(data,(err,res)=>{
-        if(err){
-            throw new Error(`${err.code}: Error parsing file. :${err.name}`)
+    console.log(`Reading ${settings.fileName}`);
+    parseString(data, (err, res) => {
+        if (err) {
+            throw new Error(`${err.code}: Error parsing file. :${err.name}`);
         }
 
-        console.log(`Parsing ${settings.fileName}`)
-        console.log("Model: ",res.flashing.header[0].phone_model[0].$.model)
+        console.log(`Parsing ${settings.fileName}`);
+        assert(
+            typeof res?.flashing?.header?.[0]?.phone_model[0]?.$?.model ===
+                'string',
+            'Expected a model number'
+        );
+        console.log('Model: ', res.flashing.header[0].phone_model[0].$.model);
 
+        assert(
+            Array.isArray(res?.flashing?.steps),
+            'Expected steps to be an array!'
+        );
         // Store steps
-        const steps = res.flashing.steps[0]
+        const steps = res.flashing.steps[0];
         //console.log(steps.$.interface)
-        if(steps.$.interface!=="AP") {
-            throw new Error("Does not look like servicefile.xml. Aborting.")
-        }
-        let sScript = ''//settings.scriptConfig.env[settings.defaultMode].preconfig + '\n'
+        assert(steps?.$?.interface === 'AP', 'Expected an Interface of AP');
+        // if (steps.$.interface !== "AP") {
+        //     throw new Error("Does not look like servicefile.xml. Aborting.");
+        // }
+        //settings.scriptConfig.env[settings.defaultMode].preconfig + '\n'
         //sScript+= `${settings.scriptConfig.env[settings.defaultMode].commentPre} Generated for ${res.flashing.header[0].phone_model[0].$.model} \n`
-        steps.step.forEach(e=>{
-            switch(e.$.operation){
-                case "oem":
-                case "getvar":
-                    sScript += (`${settings.scriptConfig.general.command} ${e.$.operation} ${e.$.var}`) +'\n'
+        const sScript = steps.step.reduce((t, e) => {
+            switch (e.$.operation) {
+                case 'oem':
+                case 'getvar':
+                    t +=
+                        `${settings.scriptConfig.general.command} ${e.$.operation} ${e.$.var}` +
+                        '\n';
                     break;
-                case "flash":
-                    sScript += (`${settings.scriptConfig.general.command} ${e.$.operation} ${e.$.partition} ${e.$.filename}`) +'\n'
+                case 'flash':
+                    t +=
+                        `${settings.scriptConfig.general.command} ${e.$.operation} ${e.$.partition} ${e.$.filename}` +
+                        '\n';
                     break;
-                case "erase":
-                    sScript += (`${settings.scriptConfig.general.command} ${e.$.operation} ${e.$.partition}`) +'\n'
+                case 'erase':
+                    t +=
+                        `${settings.scriptConfig.general.command} ${e.$.operation} ${e.$.partition}` +
+                        '\n';
                     break;
                 default:
-                    throw new Error(`Unkown: ${e.$.operation}`)
+                    throw new Error(`Unkown: ${e.$.operation}`);
             }
-        })
-        settings.scriptConfig.env.forEach(e=>{
-            const data = e.preConfig+'\n'+ e.commentPre + ` Generated for ${res.flashing.header[0].phone_model[0].$.model}` +'\n'+sScript
-            fs.writeFile(settings.serviceScript+e.extension,data,{mode:0o765},(err)=>{
-                if(err){
-                    throw new Error(`${err.errno}: Error Writing Script: ${err.name}`)
+            return t;
+        });
+
+        //Write the data down
+        settings.scriptConfig.env.forEach((e) => {
+            const data =
+                e.preConfig +
+                '\n' +
+                `${e.commentPre} Generated for ${res.flashing.header[0].phone_model[0].$.model}` +
+                '\n' +
+                sScript;
+            fs.writeFile(
+                settings.serviceScript + e.extension,
+                data,
+                { mode: 0o765 },
+                (err) => {
+                    if (err) {
+                        throw new Error(
+                            `${err.errno}: Error Writing Script: ${err.name}`
+                        );
+                    }
+                    console.log(
+                        `Done: ${settings.serviceScript + e.extension}`
+                    );
                 }
-                console.log(`Done: ${settings.serviceScript+e.extension}`)
-            })
-        })
+            );
+        });
         //console.log(sScript)
-    })
-})
+    });
+});
