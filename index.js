3db167a25135d7b95497d05ab4038cec0d90223f
Christopher Rose <christophervrose@gmail.com>
2022-01-30 13:45:12 +0530

    [fix] unstringify

--- index.js
+++ index.js
@@ -3,34 +3,50 @@ const path = require('path');
 const fs = require('fs');
 const assert = require('assert');
 
-
-
-console.log('Loading configuration: settings.json')
+// load in settings
+console.log('Loading configuration: settings.json');
 const settings = JSON.parse(
     fs.readFileSync(path.join(__dirname, 'settings.json'))
 );
-assert(typeof settings?.scriptConfig?.general?.command ==='string','Expected command')
-assert(Array.isArray(settings?.scriptConfig?.env),'Expected an array of environment statuses')
-assert(typeof settings?.fileName==='string', 'Expected a filename');
-assert(typeof settings?.serviceScript==='string','Expected a result filename')
 
+// Assert everything is alright with the settings
+assert(
+    typeof settings?.scriptConfig?.general?.command === 'string',
+    'Expected command'
+);
+assert(
+    Array.isArray(settings?.scriptConfig?.env),
+    'Expected an array of environment statuses'
+);
+assert(typeof settings?.fileName === 'string', 'Expected a filename');
+assert(
+    typeof settings?.serviceScript === 'string',
+    'Expected a result filename'
+);
+
+/** @type {string} */
+const fastbootCommand = settings.scriptConfig.general.command;
+
+/** Input File Name used for config */
+const filePathString = process.argv[2] ?? settings.fileName;
+/** Where the input file is located */
+const fileDirPath = path.dirname(filePathString);
 
 console.log('Loaded configuration');
-//console.log(settings)
-//console.log(process.argv)
-fs.readFile(settings.fileName, (err, data) => {
+
+fs.readFile(filePathString, (err, data) => {
     if (err) {
         throw new Error(
             `${err.code}: Could not read file. Try again. ${err.name}`
         );
     }
-    console.log(`Reading ${settings.fileName}`);
+    console.log(`Reading ${filePathString}`);
     parseString(data, (err, res) => {
         if (err) {
             throw new Error(`${err.code}: Error parsing file. :${err.name}`);
         }
 
-        console.log(`Parsing ${settings.fileName}`);
+        console.log(`Parsed ${filePathString}`);
         assert(
             typeof res?.flashing?.header?.[0]?.phone_model[0]?.$?.model ===
                 'string',
@@ -55,50 +71,40 @@ fs.readFile(settings.fileName, (err, data) => {
             switch (e.$.operation) {
                 case 'oem':
                 case 'getvar':
-                    t +=
-                        `${settings.scriptConfig.general.command} ${e.$.operation} ${e.$.var}` +
-                        '\n';
+                    t += `${fastbootCommand} ${e.$.operation} ${e.$.var}\n`;
                     break;
                 case 'flash':
-                    t +=
-                        `${settings.scriptConfig.general.command} ${e.$.operation} ${e.$.partition} ${e.$.filename}` +
-                        '\n';
+                    t += `${fastbootCommand} ${e.$.operation} ${e.$.partition} ${e.$.filename}\n`;
                     break;
                 case 'erase':
-                    t +=
-                        `${settings.scriptConfig.general.command} ${e.$.operation} ${e.$.partition}` +
-                        '\n';
+                    t += `${fastbootCommand} ${e.$.operation} ${e.$.partition} \n`;
                     break;
                 default:
                     throw new Error(`Unkown: ${e.$.operation}`);
             }
             return t;
-        });
+        }, '');
 
         //Write the data down
         settings.scriptConfig.env.forEach((e) => {
+            // add some spice to the script that is required
             const data =
                 e.preConfig +
-                '\n' +
-                `${e.commentPre} Generated for ${res.flashing.header[0].phone_model[0].$.model}` +
-                '\n' +
+                `\n${e.commentPre} Generated for ${res.flashing.header[0].phone_model[0].$.model} \n` +
                 sScript;
-            fs.writeFile(
-                settings.serviceScript + e.extension,
-                data,
-                { mode: 0o765 },
-                (err) => {
-                    if (err) {
-                        throw new Error(
-                            `${err.errno}: Error Writing Script: ${err.name}`
-                        );
-                    }
-                    console.log(
-                        `Done: ${settings.serviceScript + e.extension}`
+
+            const outputFileName = settings.serviceScript + e.extension;
+            /** Output file full path */
+            const resFileLocation = path.join(fileDirPath, outputFileName);
+            // write it down
+            fs.writeFile(resFileLocation, data, { mode: 0o765 }, (err) => {
+                if (err) {
+                    throw new Error(
+                        `${err.errno}: Error Writing Script: ${err.name}`
                     );
                 }
-            );
+                console.log(`Done: ${settings.serviceScript + e.extension}`);
+            });
         });
-        //console.log(sScript)
     });
 });
